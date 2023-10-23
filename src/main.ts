a448df51fe66eb5d3f10911d64033d39113e66b9
augusto <augustosaaude@gmail.com>
2023-10-23 15:06:01 -0300

    feat: add security in tests

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const address = new Address("Miguel aragão", 1160, "fortaleza", "60765065");
 customer.addAddress(address);
 customer.activate();
 
-const item1 = new OrderItem("1", "Item1", 10);
-const item2 = new OrderItem("2", "Item2", 20);
+const item1 = new OrderItem("1", "Item1", 20, "p1", 2);
+const item2 = new OrderItem("2", "Item2", 20, "p2", 3);
 
 const order = new Order("1", "123", [item1, item2])
